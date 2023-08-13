66ddfc66b46b4362e6b3bdf8ed0a446fcb00ab7d
Hridoy <ridoy.java@gmail.com>
2023-08-13 12:00:41 +0600

    Sorting Table

---
+++ src/components/columns.js
@@ -0,0 +1,77 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    Footer: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    Footer: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Email",
+    Footer: "Email",
+    accessor: "email",
+  },
+  {
+    Header: "Gender",
+    Footer: "Gender",
+    accessor: "gender",
+  },
+  {
+    Header: "Ip",
+    Footer: "Ip",
+    accessor: "ip_address",
+  },
+];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Email",
+        Footer: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Gender",
+        Footer: "Gender",
+        accessor: "gender",
+      },
+      {
+        Header: "Ip",
+        Footer: "Ip",
+        accessor: "ip_address",
+      },
+    ],
+  },
+];
